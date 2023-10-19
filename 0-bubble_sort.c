988f82702e87e4b4e1cfbe07e7d31562c82ec14b
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2023-10-19 21:43:55 +0000

    fixed up to task 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,13 +8,11 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	size_t j;
-	int temp;
-	int control;
+	size_t i, j;
+	int temp, control;
 
-	/*if (size <= 1)*/
-	/*return;*/
+	if (size <= 1)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		control = 0;
@@ -31,6 +29,5 @@ void bubble_sort(int *array, size_t size)
 		}
 		if (control == 0)
 			break;
-
 	}
 }
